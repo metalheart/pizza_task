4dacde4d7ace40dcc80f4f8d56e8ca33a2fa6933
metalheart <maksym.antipov@gmail.com>
2017-02-21 22:31:24 +0200

    IO: reading input

---
+++ pizza/Pizza.h
@@ -0,0 +1,113 @@
+#pragma once
+
+#include <vector>
+#include <algorithm>
+#include <iostream>>
+
+enum class IngradientType
+{
+	None = 0,
+	Tomato,
+	Mashroom
+};
+
+IngradientType convert(char c)
+{
+	switch(c)
+	{
+	case 'T': return IngradientType::Tomato;
+	case 'M': return IngradientType::Mashroom;
+	default: return IngradientType::None;
+	}
+}
+
+template <class T>
+using VectorOfVector = std::vector<std::vector<T>>;
+
+template <class T, class C = unsigned char>
+struct Pizza
+{
+	void allocate(size_t row, size_t col)
+	{
+		data.resize(row);
+		std::for_each(data.begin(), data.end(), [&col](std::vector<T>& row) {row.resize(col);});
+	}
+
+	// row x column
+	VectorOfVector<T> data;
+	size_t min_ingradients;
+	size_t max_cells;
+
+	std::vector<T> row(size_t row)
+	{
+		return data.at(row);
+	}
+
+	std::vector<T> col(size_t col)
+	{
+		std::vector<T> result;
+		
+		for (const ayot& row : data)
+		{
+			result.push_back(row.at(col));
+		}
+
+		return result;
+	}
+
+	VectorOfVector<C> ingradient_table(const T& type)
+	{
+		VectorOfVector<C> result;
+		result.reserve(data.size());
+
+		for (const auto& row : data)
+		{
+			std::vector<C> col;
+			col.reserve(row.size());
+
+			std::for_each(row.begin(), row.end(), 
+				[&col,&type](const T& t)
+				{
+					col.push_back(t == type ? 1 : 0);
+				}
+			);
+
+			result.push_back(std::move(col));
+		}
+
+		return result;
+
+	}
+};
+
+template <class T, class C>
+std::istream& operator >> (std::istream& stream, Pizza<T,C>& pizza)
+{
+	size_t row = 0, col = 0, min_ingradients = 0, max_cells = 0;
+	stream >> row >> col >> min_ingradients >> max_cells;
+
+	pizza.min_ingradients = min_ingradients;
+	pizza.max_cells = max_cells;
+
+	char buff[1024];
+
+	stream.ignore();
+
+	while (!stream.eof())
+	{
+		stream.getline(buff, 1024);
+
+		std::vector<T> row;
+
+		char *ptr = buff;
+		while (*ptr)
+		{
+			row.push_back(convert(*ptr));
+			++ptr;
+		}
+
+		pizza.data.push_back(std::move(row));
+	}
+
+	return stream;
+}
--- pizza/main.cpp
+++ pizza/main.cpp
@@ -1,10 +1,16 @@
+#include "Pizza.h"
+
 #include <iostream>
 #include <fstream>
 
 int main()
 {
 	using namespace std;
+	Pizza<IngradientType> pizza;
+	auto tomatos = pizza.ingradient_table(IngradientType::Tomato);
 
-	ifstream
+	ifstream input("data.txt");
+
+	input >> pizza;
 
 }
