b30332d6fc2571cd7527d145e71f0123f3678f27
mishander <malutin@gmail.com>
2017-02-21 23:31:57 +0200

    M2

--- pizza/MikeAlgo.h
+++ pizza/MikeAlgo.h
@@ -3,18 +3,24 @@
 
 std::vector<Slice> get_by_simple_rects(const Pizza<IngradientType>& pizza, size_t w, size_t h)
 {
-    size_t cw = 0, ch = 0;
+    size_t i = 0, j = 0;
     std::vector<Slice> result;
-    float gsize = pizza.data[0].size();
-    float vsize = pizza.data.size();
-    int imax = (gsize / w + 0.5);
-    int jmax = (vsize / h + 0.5);
-    for (int i = 0; i < imax; ++i)
-        for (int j = 0; j < jmax; ++i)
+    while (i < pizza.data[0].size())
+    {
+        while (j < pizza.data.size())
         {
-            Point p1 = Point(std::min(i*w, (size_t)gsize), std::min(j*h, (size_t)vsize));
-            Point p2 = Point(std::min(i*w, (size_t)gsize), std::min(j*h, (size_t)vsize));
-            pizza.ingridient_count(IngradientType::Tomato, Slice(p1, p2));
+            Point p1 = Point(i, j);
+            Point p2 = Point(i + w, j + h);
+            size_t tmcnt = pizza.ingridient_count(IngradientType::Tomato, Slice(p1, p2));
+            size_t mshcnt = pizza.ingridient_count(IngradientType::Mashroom, Slice(p1, p2));
+            if ((tmcnt < pizza.min_ingradients) && (mshcnt < pizza.min_ingradients))
+            {
+                result.push_back(Slice(p1, p2));
+            }
+            i += w;
+            j += h;
         }
+    }
+
     return result;
 }
