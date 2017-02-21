52646b3ba4587e7f5c30e6ebdaa2ed23f6d85ac8
mishander <malutin@gmail.com>
2017-02-21 23:41:00 +0200

    m3

--- pizza/MikeAlgo.h
+++ pizza/MikeAlgo.h
@@ -3,24 +3,19 @@
 
 std::vector<Slice> get_by_simple_rects(const Pizza<IngradientType>& pizza, size_t w, size_t h)
 {
-    size_t i = 0, j = 0;
     std::vector<Slice> result;
-    while (i < pizza.data[0].size())
-    {
-        while (j < pizza.data.size())
+    for(size_t i = 0; i < pizza.data[0].size(); i += w - 1)
+        for (size_t j = 0; j < pizza.data.size(); i += w - 1)
         {
             Point p1 = Point(i, j);
             Point p2 = Point(i + w, j + h);
             size_t tmcnt = pizza.ingridient_count(IngradientType::Tomato, Slice(p1, p2));
             size_t mshcnt = pizza.ingridient_count(IngradientType::Mashroom, Slice(p1, p2));
-            if ((tmcnt < pizza.min_ingradients) && (mshcnt < pizza.min_ingradients))
+            if ((tmcnt >= pizza.min_ingradients) && (mshcnt >= pizza.min_ingradients))
             {
                 result.push_back(Slice(p1, p2));
             }
-            i += w;
-            j += h;
         }
-    }
 
     return result;
 }
