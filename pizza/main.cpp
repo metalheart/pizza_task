0ffd76714551d2f329ca5868f5938af4b2a9fff6
recar <olegkuznecov@gmail.com>
2017-02-21 22:49:22 +0200

    111

--- pizza/main.cpp
+++ pizza/main.cpp
@@ -3,6 +3,19 @@
 #include <iostream>
 #include <fstream>
 
+void output_slices( const std::vector<Slice>& slices ) {
+  std::cout << slices.size() << std::endl;
+  for ( const auto& s : slices ) {
+    std::cout << s.tl.y << " " << s.tl.x << " " << s.br.y << " " << s.br.y << std::endl;
+  }
+}
+
+std::vector<Slice> get_by_simple_rects( const Pizza<IngradientType>& pizza, int w, int h ) 
+{
+  std::vector<Slice> result;
+  return result;
+}
+
 int main()
 {
 	using namespace std;
