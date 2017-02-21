27a1f70e694964ccc42afcaf08115766744a32e1
recar <olegkuznecov@gmail.com>
2017-02-21 23:53:52 +0200

    3

--- pizza/MikeAlgo.h
+++ pizza/MikeAlgo.h
@@ -4,8 +4,8 @@
 std::vector<Slice> get_by_simple_rects(const Pizza<IngradientType>& pizza, size_t w, size_t h)
 {
     std::vector<Slice> result;
-    for(size_t i = 0; i < pizza.data[0].size(); i += w)
-        for (size_t j = 0; j < pizza.data.size(); j += h)
+    for(size_t i = 0; i < pizza.data[0].size() - w; i += w)
+        for (size_t j = 0; j < pizza.data.size() - h; j += h)
         {
             Slice s(Point(i, j), Point(i + w - 1, j + h - 1));
             size_t tmcnt = pizza.ingridient_count(IngradientType::Tomato, s);
