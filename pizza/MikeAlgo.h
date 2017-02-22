d0e1fb0591d2e6bee31b070f7e6af961f5144ec3
mishander <malutin@gmail.com>
2017-02-22 12:20:06 +0200

    m6

--- pizza/MikeAlgo.h
+++ pizza/MikeAlgo.h
@@ -1,19 +1,53 @@
 #pragma once
 #include "Pizza.h"
 
-std::vector<Slice> get_by_simple_rects(const Pizza<IngradientType>& pizza, size_t w, size_t h)
+std::vector<Slice> get_by_simple_rects(const Pizza<IngradientType>& pizza, int w, int h)
 {
+    int xmax = (int)pizza.data[0].size();
+    int ymax = (int)pizza.data.size();
     std::vector<Slice> result;
-    for( int i = 0; i + w < pizza.data[0].size(); i += w)
-        for ( int j = 0; j + h < pizza.data.size(); j += h)
+    auto checkPR = [&](const Slice & s)
+    {
+        size_t tmcnt = pizza.ingridient_count(IngradientType::Tomato, s);
+        size_t mshcnt = pizza.ingridient_count(IngradientType::Mashroom, s);
+        if ((tmcnt >= pizza.min_ingradients) && (mshcnt >= pizza.min_ingradients))
+        {
+            result.emplace_back(s);
+        }
+    };
+
+    int i = 0, j = 0;
+    for(i = 0; i + w <= xmax; i += w)
+        for (j = 0; j + h <= ymax; j += h)
         {
             Slice s(Point(i, j), Point(i + w - 1, j + h - 1));
-            size_t tmcnt = pizza.ingridient_count(IngradientType::Tomato, s);
-            size_t mshcnt = pizza.ingridient_count(IngradientType::Mashroom, s);
-            if ((tmcnt >= pizza.min_ingradients) && (mshcnt >= pizza.min_ingradients))
-            {
-                result.push_back(s);
-            }
+            checkPR(s);
         }
+
+    if (i < xmax - 1)
+    {
+        for (int y = 0; y + h <= ymax; y += h)
+        {
+            Slice s(Point(i, y), Point(xmax - 1, y + h - 1));
+            checkPR(s);
+        }
+    }
+    if (j < ymax - 1)
+    {
+        for (int x = 0; x + w <= xmax; x += w)
+        {
+            Slice s(Point(x, j), Point(x + w - 1, ymax - 1));
+            checkPR(s);
+        }
+    }
+
+
+    if ( (i < xmax - 1) && (j < ymax - 1) )
+    {
+        Slice s(Point(i, j), Point(xmax - 1, ymax - 1));
+        checkPR(s);
+    }
+
+
     return result;
 }
